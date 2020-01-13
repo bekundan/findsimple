13a4c442f0d4811e687e014909ef315f72be1437
kundan <gkundan525@gmail.com>
2020-01-13 21:34:56 +0530

    remove padding of nav a

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 	
 		$(".intro-img").fadeIn(5000);
 
-		$("nav a").click(function(e){
+		$("nav a,.arrow_icon a").click(function(e){
 			e.preventDefault();
 			target=$(this.hash).offset().top;
 			$("html").animate({
 				scrollTop:target
 			},4000);	
 		})
+		
 });
