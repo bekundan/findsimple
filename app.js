3c1e5bb871157506f3b55cc6da746a6a2a9aa206
kundan <gkundan525@gmail.com>
2020-01-23 00:03:03 +0530

    chane hover

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 		
 	
-		$(".intro-img").fadeIn(5000);
-
+//		
+//    $('body').click(function(){
+//        $("#introimg").fadeIn(5000);
+//        $('.arrow_icon a').animate({
+//            color:'red'
+//        })
+//    })
+//        $(".submit_btn").click(function(){
+//            
+//           alert("demo"); 
+//        });
+       
 		$("nav a,.arrow_icon a").click(function(e){
 			e.preventDefault();
 			target=$(this.hash).offset().top;
@@ -10,5 +20,10 @@ $(document).ready(function(){
 				scrollTop:target
 			},4000);	
 		})
-		
+    $('html,body').scrollTop(0);
+		$(window).on('load',function(){
+            setTimeout(function(){
+                $('html,body').scrollTop(0);
+            },100);
+        });
 });
