e788453b1b9de2e5987cc7fe7ca22332207451f9
kundan <gkundan525@gmail.com>
2020-01-29 19:46:55 +0530

    blinking on hover

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
 $(document).ready(function(){
 		
-	
-//		
-//    $('body').click(function(){
-//        $("#introimg").fadeIn(5000);
-//        $('.arrow_icon a').animate({
-//            color:'red'
-//        })
-//    })
-//        $(".submit_btn").click(function(){
-//            
-//           alert("demo"); 
-//        });
-		// $(".project_card img").hover(function(){
-		// 	$(".card_overlay").css({
-		// 		display:"block",
-		// 		cursur:"pointer"
-		// 	});
-		// 		$(".card_overlay").mouseleave(function(){
-		// 			$(this).css({
-		// 		display:"none"
-		// 	});
-		// 		})
-		// 	});
-
        
 		$("nav a,.arrow_icon a").click(function(e){
 			e.preventDefault();
@@ -31,7 +7,7 @@ $(document).ready(function(){
 			$("html").animate({
 				scrollTop:target
 			},4000);	
-		})
+		});
     $('html,body').scrollTop(0);
 		$(window).on('load',function(){
             setTimeout(function(){
